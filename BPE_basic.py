e519662979ff28ad385d2cda6c0db9e5d44d73b9
Ronit Aryan <shadowsouledx@gmail.com>
2024-03-06 18:08:34 +0530

    completed regex based tokeniser in BPR_basic

--- BPE_basic.py
+++ BPE_basic.py
@@ -4,32 +4,42 @@ Minimal (byte-level) Byte Pair Encoding tokenizer.
 Algorithmically follows along the GPT tokenizer:
 https://github.com/openai/gpt-2/blob/master/src/encoder.py
 
-But:
-- Does not handle the regular expression splitting pattern.
-- Does not handle any special tokens.
+Uses GPT4 regex pattern by default
 """
-
+import regex as re
 from .Tokenizer import Tokenizer
 from .helpers import get_pair_counts, merge
 
-class BasicTokenizerBPE(Tokenizer):
-    def __init__(self) -> None:
+# the main GPT text split patterns, see
+# https://github.com/openai/tiktoken/blob/main/tiktoken_ext/openai_public.py
+GPT2_SPLIT_PATTERN = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+GPT4_SPLIT_PATTERN = r"""'(?i:[sdmt]|ll|ve|re)|[^\r\n\p{L}\p{N}]?+\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]++[\r\n]*|\s*[\r\n]|\s+(?!\S)|\s+"""
+
+
+
+class TokenizerBPE(Tokenizer):
+    def __init__(self, pattern=None) -> None:
         super().__init__()
+        self.pattern = GPT4_SPLIT_PATTERN if pattern is None else pattern
+        self.compiled_pattern = re.compile(self.pattern)
+        self.special_tokens = {}
+        self.inverse_special_tokens = {}
 
     def train(self, text, vocab_size, verbose=False):
         assert vocab_size >= 256
         num_merges = vocab_size - 256
+         # split the text into chunks using the pattern
+        text_re = re.findall(self.compiled_pattern, text)  
+        #  convert into ids list
+        ids = list(ch.encode('utf08') for ch in text_re)
 
-        text_bytes = text.encode("utf-8") # raw bytes
-        ids = list(text_bytes) # list of integers in range 0..255
-
+        # iteratively merge the most common pairs 
         merges = {}
         vocab = {idx: bytes([idx]) for idx in range(256)}
-
         for i in range(num_merges):
             stats = get_pair_counts(ids)
             pair = max(stats, key=stats.get)
-            idx = 256 + i
+            idx = 256 + i   # mint a new token
             ids = merge(ids, pair, idx)
             merges[pair] = idx
             vocab[idx] = vocab[pair[0]] + vocab[pair[1]]
@@ -38,22 +48,73 @@ class BasicTokenizerBPE(Tokenizer):
             
         self.merges = merges
         self.vocab = vocab
-    
-    def decode(self, ids):
-        text_bytes = b"".join(self.vocab[idx] for idx in ids)
-        text = text_bytes.decode('utf-8', errors='replace') # we use errors='replace' to handle invalid tokens
-        return text
 
-    def encode(self, text):
-        text_bytes = text.encode('utf-8')
+    def register_special_tokens(self, special_tokens):
+        # special_tokens is a dictionary of str -> int
+        self.special_tokens = special_tokens
+        self.inverse_special_tokens = {v: k for k, v in special_tokens.items()}
+
+    def decode(self, ids):
+        part_bytes = []
+
+        for idx in ids:
+            if idx in self.vocab:
+                part_bytes.append(self.vocab[idx])
+            elif idx in self.inverse_special_tokens:
+                part_bytes.append(self.inverse_special_tokens[idx].encode('utf-8'))
+            else:
+                raise ValueError(f'Invalid token id: {idx}')
+            
+        text_bytes = b"".join(part_bytes)
+        text = text_bytes.decode('utf-8', errors='replace') # we use errors='replace' to handle invalid tokens
+        return text 
+    
+    def _encode_chunk(self, text_bytes):
         ids = list(text_bytes)
+
         while len(ids) >= 2:
             stats = get_pair_counts(ids)
-            pair = min(stats, key=lambda p: self.merges.get(p, float('inf')))
-
+            pair = min(stats, key=lambda p:self.merges.get(p, float('inf')))
             if pair not in self.merges:
                 break
-        
             idx = self.merges[pair]
             ids = merge(ids, pair, idx)
+        return ids
+    
+    def encode_ordinary(self, text):
+        text_chunks = re.findall(self.compiled_pattern, text)
+        ids = []
+        for chunk in text_chunks:
+            chunk_bytes = chunk.encode('utf-8')
+            chunk_ids = self._encode_chunk(chunk_bytes)
+            ids.extend(chunk_ids)
+        return ids
+
+    def encode(self, text, allowed_special="none_raise"):
+        special = None
+        if allowed_special == "all":
+            special = self.special_tokens
+        elif allowed_special == "none":
+            special = {}
+        elif allowed_special == "none_raise":
+            special = {}
+            assert all(token not in text for token in self.special_tokens)
+        elif isinstance(allowed_special, set):
+            special = {k: v for k, v in self.special_tokens.items() if k in allowed_special}
+        else:
+            raise ValueError(f"allowed_special={allowed_special} not understood")
+        
+        if not special:
+            return self.encode_ordinary(text)
+        
+        # split all special into different chunks
+        special_pattern = "(" + "|".join(re.escape(k) for k in special) + ")"
+        special_chunks = re.split(special_pattern, text)
+
+        ids = []
+        for part in special_chunks:
+            if part in special:
+                ids.append(special[part])
+            else:
+                ids.extend(self.encode_ordinary(part))
         return ids
